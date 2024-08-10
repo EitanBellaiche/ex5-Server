2e236e232fad1eb74942e2ac8bd81f813227f0c6
eitanBellaiche <eitanBellaiche@gmail.com>
2024-08-10 20:53:38 +0300

    change port

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 require('dotenv').config(); 
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 
 const playersRouter = require('./routers/playersRouter');
 
 
-app.use(express.json());
+const corsOptions = {
+  origin: 'https://ex5-server.onrender.com', 
+  optionsSuccessStatus: 200
+};
 
+app.use(cors(corsOptions)); 
+
+app.use(express.json());
 
 app.use('/api/players', playersRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000; 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
