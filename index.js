c1348013b5b124945f8d0ca6d62376678c63cd40
eitanBellaiche <eitanBellaiche@gmail.com>
2024-08-10 21:05:27 +0300

    update index

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 require('dotenv').config(); 
 const express = require('express');
-const cors = require('cors'); 
 const app = express();
 
-const playersRouter = require('./routers/playersRouter');
+const playersRouter = require('./routers/playersRouter'); 
 
-
-const corsOptions = {
-  origin: 'https://ex5-server.onrender.com', 
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions)); 
+const port = process.env.PORT || 10000; 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+    res.set({
+        'Access-Control-Allow-Origin': 'https://ex5-server.onrender.com', 
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': "GET, POST, PUT, DELETE",
+        'Content-Type': 'application/json'
+    });
+    next();
+});
+
 
 app.use('/api/players', playersRouter);
 
-const port = process.env.PORT || 10000; 
+
+app.use((req, res) => {
+    console.error('Path not found:', req.path);
+    res.status(400).send('Something is broken!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
