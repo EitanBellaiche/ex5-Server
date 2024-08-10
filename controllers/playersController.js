36871dd2f2196d410b074c8f2a4e1a4e847aedb2
eitanBellaiche <eitanBellaiche@gmail.com>
2024-08-11 01:01:38 +0300

    try GET

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -1,13 +1,11 @@
 exports.playersController = {
     async addPlayer(req, res) {
         try {
-     
             console.log('Received data:', req.body);
 
             const { dbConnection } = require('../db_connection');
             const { player_name, player_goals, player_match_played, player_description } = req.body;
 
-          
             if (!player_name || !player_goals || !player_match_played || !player_description) {
                 console.log('Missing fields:', {
                     player_name,
@@ -39,6 +37,22 @@ exports.playersController = {
         } catch (error) {
             console.error('Error adding or updating player:', error);
             res.status(500).json({ success: false, message: 'Failed to add or update player', error: error.message });
+        }
+    },
+
+    async getPlayers(req, res) {
+        try {
+            const { dbConnection } = require('../db_connection');
+            const connection = await dbConnection.createConnection();
+
+            const [players] = await connection.execute('SELECT player_name, player_goals, player_match_played, player_description FROM tbl_15_players');
+            
+            connection.end();
+
+            res.json(players);
+        } catch (error) {
+            console.error('Error fetching players:', error);
+            res.status(500).json({ success: false, message: 'Failed to fetch players', error: error.message });
         }
     }
 };
--- routers/playersRouter.js
+++ routers/playersRouter.js
@@ -3,7 +3,7 @@ const { playersController } = require('../controllers/playersController.js');
 
 const playersRouter = new Router(); 
 
-
+playersRouter.get('/', playersController.getPlayers);
 playersRouter.post('/add', (req, res, next) => {
     console.log("Received POST request at /api/players/add");
     next();
