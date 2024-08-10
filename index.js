a12e0d521983a941671a3e8c3a2893939f76f50b
eitanBellaiche <eitanBellaiche@gmail.com>
2024-08-10 20:33:59 +0300

    fix cors

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 require('dotenv').config(); 
 const express = require('express');
-const cors = require('cors'); 
 const app = express();
 
-const playersRouter = require('./routers/playersRouter'); 
-
-
-const corsOptions = {
-  origin: 'https://ex5-server.onrender.com', 
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions)); 
+const playersRouter = require('./routers/playersRouter');
 
 
 app.use(express.json());
@@ -19,7 +10,7 @@ app.use(express.json());
 
 app.use('/api/players', playersRouter);
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
